bfdf90dcaf1e55d9bcd09203d1d1a5bc37ab7468
KydyrovErjan <erjankydyrov22@gmail.com>
2021-12-01 01:16:20 +0600

    created Post, Commet and Like models

--- post/models.py
+++ post/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+LIKE_CHOICES = (
+    ('like', 'like'),
+    ('dislike', 'dislike')
+)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE)
+    posted_time = models.DateTimeField(auto_now=True)
+    article = models.CharField(max_length=100)
+    text = models.TextField()
+    additional_file = models.FileField(blank=True)
+
+    def __str__(self):
+        return self.article
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE)
+    value = models.CharField(max_length=10, choices=LIKE_CHOICES)
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Post,
+                                on_delete=models.CASCADE)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE)
+    text = models.TextField()
+
+
+class CommentLike(models.Model):
+    comment = models.ForeignKey(Comment,
+                                on_delete=models.CASCADE)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE)
+    value = models.CharField(max_length=10, choices=LIKE_CHOICES)
+
