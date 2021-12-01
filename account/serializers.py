7fdca8d9b75dd75cf9c60140eabce168916a07d3
KydyrovErjan <erjankydyrov22@gmail.com>
2021-12-01 22:11:06 +0600

    modified serializers

--- account/serializers.py
+++ account/serializers.py
@@ -1,16 +1,71 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 
 User = get_user_model()
 
 
 class RegistrationSerializer(serializers.Serializer):
-    pass
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True, min_length=6)
+    password_confirm = serializers.CharField(required=True, min_length=6)
+    name = serializers.CharField(required=True)
+
+    def validate_email(self, email):
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError('Пользователь с таким email уже зарегистрирован')
+        return email
+
+    def validate(self, data):
+        password = data.get('password')
+        password_confirm = data.pop('password_confirm')
+        if password != password_confirm:
+            raise serializers.ValidationError('Пароли должны совпадать')
+        return data
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        user.create_activation_code()
+        user.send_activation_email()
 
 
 class ActivationSerializer(serializers.Serializer):
-    pass
+    email = serializers.EmailField()
+    code = serializers.CharField()
+
+    def validate(self, data):
+        email = data.get('email')
+        code = data.get('code')
+        if not User.objects.filter(email=email,
+                                   activation_code=code).exists():
+            raise serializers.ValidationError('Пользователь не найден')
+        return data
+
+    def activate(self):
+        email = self.validated_data.get('email')
+        user = User.objects.get(email=email)
+        user.is_active = True
+        user.activation_code = ''
+        user.save()
 
 
 class LoginSerializer(serializers.Serializer):
-    pass
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True)
+
+    def validate_email(self, email):
+        if not User.objects.filter(email=email).exists():
+            raise serializers.ValidationError('Пользователь не найден')
+        return email
+
+    def validate(self, data):
+        request = self.context.get('request')
+        email = data.get('email')
+        password = data.get('password')
+        if email and password:
+            user = authenticate(username=email,
+                                password=password,
+                                request=request)
+            if not user:
+                raise serializers.ValidationError('Неверный email или пароль')
+            data['user'] = user
+            return data
