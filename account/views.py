f1144abe0214dfa2be6519375b6fe00b92ca266b
KydyrovErjan <erjankydyrov22@gmail.com>
2021-12-01 21:19:07 +0600

    created views

--- account/views.py
+++ account/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from account.serializers import *
+
+
+class Registration(APIView):
+
+    def post(self, request):
+        serializer = RegistrationSerializer
+        if serializer.is_valid(raise_exception=True):
+            serializer.create(serializer.validated_data)
+            return Response('Аккаунт успешно зарегистрирован', status=201)
+
+
+class Activation(APIView):
+
+    def post(self, request):
+        serializer = ActivationSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.activated()
+            return Response('Аккаунт успешно активирован', status=200)
+
+
+class LoginView(ObtainAuthToken):
+    serializer_class = LoginSerializer
+
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        Token.objects.filter(user=user).delete()
+        return Response('Вы успешно разлогинились')
--- post/views.py
+++ post/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+
 from .models import *
+from .permissions import IsAuthorOrAdmin
+from .serializers import PostSerializer
 
 
 class PostViewSet(ModelViewSet):
