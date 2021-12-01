00c051f39290e8bf9339be3803645965a27cead5
KydyrovErjan <erjankydyrov22@gmail.com>
2021-12-01 11:07:00 +0600

    created PostViewSet

--- post/views.py
+++ post/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from .models import *
 
-# Create your views here.
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [IsAuthenticated()]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            return [IsAuthenticated(), IsAuthorOrAdmin()]
+        return []
